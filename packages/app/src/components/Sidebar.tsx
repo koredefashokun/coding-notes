69f2d5454a165eccf44e76c5f0598419a206f4e7
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-06-23 17:05:26 +0100

    Connect and render

--- packages/app/src/components/Sidebar.tsx
+++ packages/app/src/components/Sidebar.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
-import { withApollo, Query } from 'react-apollo';
+import { Query } from 'react-apollo';
 import { SidebarContainer } from './elements';
 import { FETCH_NOTES } from '../graphql/queries';
+
+interface Note {
+	_id: string;
+	title: string;
+}
 
 interface SidebarProps {
 	collapsed: boolean;
 }
 
 const Sidebar = ({ collapsed }: SidebarProps) => (
-	<Query query={FETCH_NOTES}>
-		{({ loading, error, data }: any) => {
+	<Query<{ notes: Note[] }> query={FETCH_NOTES}>
+		{({ loading, error, data }) => {
 			if (error) return <p>Error</p>;
 			if (loading) return <p>Loading</p>;
 			console.log(data);
-			return <SidebarContainer />;
+			return (
+				data && (
+					<SidebarContainer>
+						{data.notes.map(note => (
+							<p>{note.title}</p>
+						))}
+					</SidebarContainer>
+				)
+			);
 		}}
 	</Query>
 );
 
-export default withApollo(Sidebar);
+export default Sidebar;
